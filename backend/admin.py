41ad567c8b7af4c116cf65805bfb841f2f36a2bd
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-16 19:59:36 +0300

    registered all other models to the admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import User, Project, Student, Supervisor, Announcement, Proposal
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Student)
+admin.site.register(Supervisor)
+admin.site.register(Project)
+admin.site.register(Announcement)
+admin.site.register(Proposal)
