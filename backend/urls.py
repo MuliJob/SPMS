fb7eca5eb796d9a061e7e003fa7966ecf102c441
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-18 17:13:43 +0300

    added a login and logout path

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, StudentViewSet, SupervisorViewSet, ProjectViewSet, ProposalViewSet, AnnouncementViewSet
+from .user_views import UserViewSet, StudentViewSet, SupervisorViewSet, ProjectViewSet, ProposalViewSet, AnnouncementViewSet
+from .views.auth import CustomAuthToken,  LogoutView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,4 +13,7 @@ router.register(r'announcements', AnnouncementViewSet, basename='announcements')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', CustomAuthToken.as_view(), name='api_login'),
+    path('logout/', LogoutView.as_view(), name='api_logout'),
+
 ]
