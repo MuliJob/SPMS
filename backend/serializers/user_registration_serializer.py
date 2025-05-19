18338dd96c59d700eb343e8cb8a684d136ebc17e
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-19 23:03:22 +0300

    An individual serializer class for all modules

---
+++ backend/serializers/user_registration_serializer.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_modelfrom 
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserRegistartionSerializer(serializers.ModelSerializer):
+    password = serial.charField(write_only+True)
+    role = serializers.ChoiceField(choices=[('student','student'), ('supervisor', 'supervisor')])
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role']
+
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            email = validated_data('email'),
+            password = validated_data['password'],
+            role= validated_data['role']
+        )
+
+        return User
