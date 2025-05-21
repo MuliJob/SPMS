42e19db17a7843847be5b9c2a42cf56ba41c9036
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-21 21:08:36 +0300

    solved errors on the user registraitio

--- backend/serializers/user_registration_serializer.py
+++ backend/serializers/user_registration_serializer.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import get_user_modelfrom 
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 User = get_user_model()
 
 
 class UserRegistartionSerializer(serializers.ModelSerializer):
-    password = serial.charField(write_only+True)
+    password = serializers.CharField(write_only=True)
     role = serializers.ChoiceField(choices=[('student','student'), ('supervisor', 'supervisor')])
 
     class Meta:
@@ -16,7 +16,7 @@ class UserRegistartionSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username = validated_data['username'],
-            email = validated_data('email'),
+            email = validated_data.get('email'),
             password = validated_data['password'],
             role= validated_data['role']
         )
