331d73d90f4a3e7c407f77069dece8606696372f
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-24 22:45:54 +0300

    created the login view class

---
+++ backend/views/login.py
@@ -0,0 +1,26 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+
+
+class LoginView(APIView):
+     def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+
+        if user:
+            token, craeted = Token.objects.get_or_create(user=user)
+            return Response({
+                "message": "Login successful",
+                "token": token.key
+            }, ststus=status.HTTP_200_OK)
+        else:
+            return Response({
+                "error": "Invalid username or password"
+            }, status=status.HTTP_401_UNAUTHORIZED)
