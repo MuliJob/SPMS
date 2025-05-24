fe090bf7e3f414a61ff1df76947b90185e46f976
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-24 20:15:10 +0300

    Changed the registration view error

--- backend/views/register.py
+++ backend/views/register.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
-from rest_framework.response import response
+from rest_framework.response import Response
 from rest_framework import status
-from backend.serializers.user_registration_serializer import UserRegistrationSerializer
+from backend.serializers.user_serializer import UserSerializer
 
 
-class RegistartionView(APIView):
+class RegistrationView(APIView):
     def post(self, request):
-        serializer =UserRegistartionSerializer(data=request.data)
+        serializer =UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
