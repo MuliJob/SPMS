c5a8ee5713abb8549019d69cf7104f2bfd30a0d1
qmuuoryan <ryanmuuo91@gmail.com>
2025-05-24 22:16:27 +0300

    tested the registration in postman and it was working fine

--- backend/views/register.py
+++ backend/views/register.py
@@ -1,14 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from backend.serializers.user_serializer import UserSerializer
-
 
 class RegistrationView(APIView):
     def post(self, request):
-        serializer =UserSerializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "message": "User registered successfully",
+                "user": UserSerializer(user).data,
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
