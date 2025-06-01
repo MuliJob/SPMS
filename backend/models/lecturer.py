6a0ca7b81cf720890262bbd97131574066ad8e75
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-01 23:33:10 +0300

    added a lecturer model who approves projects and assign students supervisors only

---
+++ backend/models/lecturer.py
@@ -0,0 +1,8 @@
+from django.db import models
+from backend.models.user import User
+
+class Lecturer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Lecturer: {self.user.username}"
