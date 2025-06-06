491803ac032410c0c03842ed8d6425da406aa2e1
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-06 22:26:47 +0300

    registered the user roles in admin to assign a role to a registered user

--- backend/admin.py
+++ backend/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-from .models import User, Project, Student, Supervisor, Announcement, Proposal
 
-admin.site.register(User, UserAdmin)
+
+from .models import User, Project, Student, Supervisor, Announcement, Proposal, Lecturer
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
+
 admin.site.register(Student)
 admin.site.register(Supervisor)
 admin.site.register(Project)
 admin.site.register(Announcement)
 admin.site.register(Proposal)
+admin.site.register(Lecturer)
