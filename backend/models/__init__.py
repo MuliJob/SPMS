4d1f20c1296c72d3236922d9f6303ec1234fabd7
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-06 22:29:08 +0300

    imported the lecturer module in the init file from the models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,3 +4,5 @@ from .supervisor import Supervisor
 from .project import Project
 from .proposal import Proposal, proposal_upload_path
 from .announcement import Announcement
+from .lecturer import Lecturer
+
