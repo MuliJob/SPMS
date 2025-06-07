3bc912c246cfce0e92ea6bd75d6f592269f0f8db
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-07 03:38:13 +0300

    Added status choices to the proposal model

--- backend/models/proposal.py
+++ backend/models/proposal.py
@@ -7,11 +7,19 @@ def proposal_upload_path(instance, filename):
 
 
 class Proposal(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'pending'),
+        ('reviewed', 'Reviewed'),
+        ('needs_revision', 'Needs Revision'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    ]
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=proposal_upload_path)
+    file = models.FileField(upload_to = proposal_upload_path)
     feedback = models.TextField(blank=True, null=True)
     submitted_at = models.DateTimeField(auto_now_add=True)
-
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
 
     def __str__(self):
         return f"proposal for - {self.project.title}"
