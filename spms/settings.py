32d1964ebc92b001a4bdacdf12f06f901c82120a
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-15 19:42:43 +0300

    updated the CORS localhost url

--- spms/settings.py
+++ spms/settings.py
@@ -131,6 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:38787',  
+    'http://127.0.0.1:8000',
+    
+]
+
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
