a144526b323e555a55e62035ac44e21fbb95315c
qmuuoryan <ryanmuuo91@gmail.com>
2025-07-17 13:25:14 +0300

    updated my settings

--- spms/settings.py
+++ spms/settings.py
@@ -170,7 +170,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://spms-frontend.web.app/',  
     'https://spms-frontend.firebaseapp.com/',  
-   ,
+   
 ]
 
 CORS_ALLOW_CREDENTIALS = True
